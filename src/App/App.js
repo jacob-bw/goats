36c59de5b358020b5f4a6f126d92e40c1f53acfd
Jacob <wittenberg.jacob@gmail.com>
2019-12-09 19:47:15 -0600

    state and props

--- src/App/App.js
+++ src/App/App.js
@@ -1,27 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.scss';
 
-function App() {
-  return (
+import goatData from '../helpers/data/goatData';
+
+import GoatCorral from '../components/goatCorral/goatCorral';
+
+class App extends React.Component {
+  state = {
+    goats: [],
+  }
+
+  componentDidMount() {
+    const goats = goatData.getGoats();
+    this.setState({ goats });
+  }
+
+  render() {
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <button className="btn btn-dark">Goats</button>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button className="btn btn-dark">Goats</button>
+      <GoatCorral butts={this.state.goats} />
     </div>
-  );
+    );
+  }
 }
 
 export default App;
